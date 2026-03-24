bdf00ca7a98b61da6bf640baacbc2c4629477097
Tenuuu <charlestenuta@gmail.com>
2026-03-24 09:47:08 -0500

    refactored the tests and added premade files + hacky staging of empty_samples folder

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -7,69 +7,48 @@ import os
 
 from app.services import conversion
 
-# change the data_folder to our samples folder for testing
-conversion.data_folder = "tests/samples/"
-
 def test_empty_parquets_exists():
-    """Tests the parquets_exists function when the samples folder is empty"""
+    """Tests the parquets_exists function when the empty_samples folder is empty"""
 
-    # should return False if no parquet files exist
-    assert not conversion.parquets_exists()
+    # should return False since no parquet files exist within the empty_samples folder
+    assert not conversion.parquets_exists("tests/empty_samples/")
 
 
 def test_incorrect_file_type_parquets_exists():
-    """Tests the parquets_exists function when only incorrect file types exist"""
+    """Tests the parquets_exists function when the incorrect_samples folder contains incorrect file types"""
 
-    # create incorrect file types in the samples folder
-    for i in range(5):
-        with open(f"{conversion.data_folder}/file_{i}.txt", "w") as f:
-            f.write("")
-
-    # should return False since no parquet files exist
-    assert not conversion.parquets_exists()
-
-    # remove the incorrect files we created so next test runs properly
-    filelist = [f for f in os.listdir(conversion.data_folder) if f.endswith(".txt")]
-    for f in filelist:
-        os.remove(os.path.join(conversion.data_folder, f))
+    # should return False since no parquet files exist within the incorrect_samples folder
+    assert not conversion.parquets_exists("tests/incorrect_samples/")
 
 
 def test_parquets_exists():
-    """Tests the parquets_exists function when parquet files exist"""
+    """Tests the parquets_exists function when the samples folder contains parquet files amongst other file types"""
 
-    # create parquet files in the samples folder
-    for i in range(5):
-        with open(f"{conversion.data_folder}/file_{i}.parquet", "w") as f:
-            f.write("")
-
-    # returns True since the parquet files were created
-    assert conversion.parquets_exists()
-
-    # create incorrect file types in the samples folder. should not affect results
-    for i in range(5):
-        with open(f"{conversion.data_folder}/file_{i}.txt", "w") as f:
-            f.write("")
+    # run the test if the parquet files already exist. most likely will not since we .gitignore .parquet files
+    if conversion.parquets_exists("tests/samples/"):
+        assert conversion.parquets_exists("tests/samples/")
+    else:
+        # create parquet files in the samples folder, then run the test
+        for i in range(5):
+            with open(f"tests/samples/file_{i}.parquet", "w") as f:
+                f.write("")
     
-    # should still return True since the parquet files exist
-    assert conversion.parquets_exists()
+        # should still return True despite there being other file types within the samples folder
+        assert conversion.parquets_exists("tests/samples/")
 
-    # remove the files we created so next test runs properly
-    filelist = [f for f in os.listdir(conversion.data_folder) if f.endswith(".parquet") or f.endswith(".txt")]
-    for f in filelist:
-        os.remove(os.path.join(conversion.data_folder, f))
 
-conversion.data_folder = "tests/csv_samples/"
+# conversion.data_folder = "tests/csv_samples/"
 
-def test_convert_to_parquet():
-    """Tests the convert_to_parquet function and checks if the data from the CSV files properly converts"""
+# def test_convert_to_parquet():
+#     """Tests the convert_to_parquet function and checks if the data from the CSV files properly converts"""
 
-    # run the conversion function on our sample data
-    conversion.convert_to_parquet()
+#     # run the conversion function on our sample data
+#     conversion.convert_to_parquet()
 
-    # should create parquet files, so parquets_exists should return True
-    assert conversion.parquets_exists()
+#     # should create parquet files, so parquets_exists should return True
+#     assert conversion.parquets_exists()
 
-    # remove the files we created so next test runs properly
-    filelist = [f for f in os.listdir(conversion.data_folder) if f.endswith(".parquet")]
-    for f in filelist:
-        os.remove(os.path.join(conversion.data_folder, f))
+#     # remove the files we created so next test runs properly
+#     filelist = [f for f in os.listdir(conversion.data_folder) if f.endswith(".parquet")]
+#     for f in filelist:
+#         os.remove(os.path.join(conversion.data_folder, f))

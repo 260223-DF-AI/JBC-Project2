6447243d76f85d3db829b2825b59609377e3ddb9
Benjamin Ledoux <bledoux2002@gmail.com>
2026-03-25 10:18:17 -0500

    Revert "first draft of sending sql query to BigQuery, need to make sql statement construction function. going to work on adding constraints to table creation (PKs FKs, etc)"
    This reverts commit a3f9b24ed533ff36a9e4d51749473d0af4a2e85e.

--- app/services/bigquery.py
+++ app/services/bigquery.py
@@ -3,7 +3,6 @@ from app.services.gcs import connect
 from datetime import datetime
 import logging
 from google.cloud import storage
-import pandas as pd
 
 logger = logging.getLogger(__name__)
 
@@ -40,51 +39,32 @@ OPTIONS (
         query_job = client.query(sql)
         query_job.result()  # Wait for the query to complete
         msg = f"External table {table} created successfully."
-        # print(msg)
+        print(msg)
         logger.info(msg)
     except Exception as e:
         msg = f"Error creating external table {table}: {e}"
-        # print(msg)
+        print(msg)
         logger.error(msg)
 
-# def update_external_table(): # necessary?
-#     """
-    
-#     """
-
-def query_bigquery(client: bigquery.Client, sql: str, project_id) -> pd.DataFrame:
-    """
-    Send SQL query to our BigQuery data warehouse, return DataFrame
-    """
-    df = pd.DataFrame()
-
-    try:
-        query_job = client.query(sql)
-        rows = query_job.result()
-        df = rows.to_dataframe()
-        msg = f"Query completed successfully"
-        logger.info(msg)
-    except Exception as e:
-        msg = f"Error with query: {e}"
-        logger.error(msg)
-
-    return df
-
-def construct_query() -> str:
-    """
-    Take in certain parameters and construct a predefined DQL statement to be sent to BigQuery
+def update_external_table():
     """
     
+    """
 
-# def get_dataset(project_id, dataset_id): # necessary?
-#     """
+def query_bigquery(sql, project_id):
+    """
     
-#     """
+    """
 
-# def partition_external_table():  # Handle year/month partitions
-#     """
+def get_dataset(project_id, dataset_id):
+    """
     
-#     """
+    """
+
+def partition_external_table():  # Handle year/month partitions
+    """
+    
+    """
 
 
 if __name__ == "__main__":
